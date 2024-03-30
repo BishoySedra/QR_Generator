49afaa680bc2d168cc620d7b860d0cc1060b38d2
Bishoy Sedra <bishosedra0@gmail.com>
2024-03-30 14:32:40 +0200

    optimize the code to make it more dynamic!

--- public/index.js
+++ public/index.js
@@ -1,19 +1,34 @@
-const nameField = document.getElementById("name");
-const emailField = document.getElementById("email");
-const phoneField = document.getElementById("phone");
-const CVField = document.getElementById("cv");
 const generateBtn = document.getElementById("submit");
 const qrCodeImage = document.getElementsByTagName("img")[0];
 const qrCodeContainer = document.getElementById("qr-code-container");
+const dataForm = document.getElementsByTagName("form");
 
 generateBtn.addEventListener("click", async (e) => {
+  // prevent the default form submission
   e.preventDefault();
-  const data = {
-    name: nameField.value,
-    email: emailField.value,
-    phone: phoneField.value,
-    cv: CVField.value,
-  };
+
+  // iterate over the form input fields and store the values in an object
+  let data = {};
+  for (let i = 0; i < dataForm[0].length; i++) {
+    if (dataForm[0][i].id === "submit") {
+      continue;
+    }
+
+    console.log(dataForm[0][i].id, dataForm[0][i].value);
+
+    if (
+      dataForm[0][i].value === "" ||
+      dataForm[0][i].value === null ||
+      dataForm[0][i].value === undefined
+    ) {
+      data[dataForm[0][i].id] = "N/A";
+      continue;
+    }
+
+    data[dataForm[0][i].id] = dataForm[0][i].value;
+  }
+
+  console.log(data);
 
   const response = await fetch("/generateQR", {
     method: "POST",
